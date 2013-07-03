086083ed61596f8d9d091610afffcb392da7ad5b
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2013-07-03 12:39:40 +0000

    ver 0.2.1 bug msgCat 002 solved
    git-svn-id: http://syspver1/svn/develop/modules/msgCat@992 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- include/ctl.h
+++ include/ctl.h
@@ -38,7 +38,7 @@
 // ---------------------------------------------------------
 #define DEFAULT_LOG_LEVEL      ERR
 #define LOG_BUFFER_LINE_SIZE   256
-#define LOG_BUFFER_CACHE_SIZE   64
+#define LOG_BUFFER_CACHE_SIZE  512
 
 // ---------------------------------------------------------
 // reserved id's
