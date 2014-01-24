fdbb7faf62ee1d80c085c65c8a1e74e0198fbddb
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2014-01-24 12:50:39 +0000

    0.5.05 additional mq message
    git-svn-id: http://syspver1/svn/develop/modules/msgCat@1788 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- include/msgcat/lgmqm.h
+++ include/msgcat/lgmqm.h
@@ -55,6 +55,10 @@
 #define LEV_LMQM_Q_OPENED        LOG
 #define TXT_LMQM_Q_OPENED        "queue %s opened"
 
+#define     LMQM_UNKNOWN_QMGR    1113
+#define LEV_LMQM_UNKNOWN_QMGR    ERR
+#define TXT_LMQM_UNKNOWN_QMGR    "queue manager %s does not exists"
+
 #define     LMQM_INCR_MSG_BUFF   1120
 #define LEV_LMQM_INCR_MSG_BUFF   INF
 #define TXT_LMQM_INCR_MSG_BUFF   "increase message buffer for MQGET to %d byte"
