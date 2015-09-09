6f40609481db7f57d90140557818e190de835f0a
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2015-09-09 17:04:20 +0000

    0.10.04 new message for writing file
    git-svn-id: http://syspver1/svn/develop/modules/msgCat@2626 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- include/msgcat/lgstd.h
+++ include/msgcat/lgstd.h
@@ -19,125 +19,129 @@
 /******************************************************************************/
 /*   D E F I N E S                                                            */
 /******************************************************************************/
-#define     LSTD_GEN_SYS            102
-#define LEV_LSTD_GEN_SYS            SYS
-#define TXT_LSTD_GEN_SYS            "%s aborted"
+#define     LSTD_GEN_SYS                  102
+#define LEV_LSTD_GEN_SYS                  SYS
+#define TXT_LSTD_GEN_SYS                  "%s aborted"
 
-#define     LSTD_GEN_CRI            103
-#define LEV_LSTD_GEN_CRI            CRI
-#define TXT_LSTD_GEN_CRI            "%s aborted"
+#define     LSTD_GEN_CRI                  103
+#define LEV_LSTD_GEN_CRI                  CRI
+#define TXT_LSTD_GEN_CRI                  "%s aborted"
 
-#define     LSTD_GEN_ERR            104 
-#define LEV_LSTD_GEN_ERR            ERR
-#define TXT_LSTD_GEN_ERR            "%s aborted"
+#define     LSTD_GEN_ERR                  104 
+#define LEV_LSTD_GEN_ERR                  ERR
+#define TXT_LSTD_GEN_ERR                  "%s aborted"
 
-#define     LSTD_ERRNO_ERR          105 
-#define LEV_LSTD_ERRNO_ERR          ERR
-#define TXT_LSTD_ERRNO_ERR          "system call failed with errno: %d %s"
+#define     LSTD_ERRNO_ERR                105 
+#define LEV_LSTD_ERRNO_ERR                ERR
+#define TXT_LSTD_ERRNO_ERR                "system call failed with errno: %d %s"
 
-#define     LSTD_ERRNO_WAR          106 
-#define LEV_LSTD_ERRNO_WAR          WAR
-#define TXT_LSTD_ERRNO_WAR          "system call failed with errno: %d %s"
+#define     LSTD_ERRNO_WAR                106 
+#define LEV_LSTD_ERRNO_WAR                WAR
+#define TXT_LSTD_ERRNO_WAR                "system call failed with errno: %d %s"
 
-#define     LSTD_ERRNO_LOG          107 
-#define LEV_LSTD_ERRNO_LOG          LOG
-#define TXT_LSTD_ERRNO_LOG          "system call failed with errno: %d %s"
+#define     LSTD_ERRNO_LOG                107 
+#define LEV_LSTD_ERRNO_LOG                LOG
+#define TXT_LSTD_ERRNO_LOG                "system call failed with errno: %d %s"
 
-#define     LSTD_OPEN_FILE_FAILED   110
-#define LEV_LSTD_OPEN_FILE_FAILED   ERR
-#define TXT_LSTD_OPEN_FILE_FAILED   "failed to open file %s"
+#define     LSTD_OPEN_FILE_FAILED         110
+#define LEV_LSTD_OPEN_FILE_FAILED         ERR
+#define TXT_LSTD_OPEN_FILE_FAILED         "failed to open file %s"
 
-#define     LSTD_GET_FILE_SIZE      111
-#define LEV_LSTD_GET_FILE_SIZE      ERR
-#define TXT_LSTD_GET_FILE_SIZE      "can not get file size for %s" 
+#define     LSTD_GET_FILE_SIZE            111
+#define LEV_LSTD_GET_FILE_SIZE            ERR
+#define TXT_LSTD_GET_FILE_SIZE            "can not get file size for %s" 
 
-#define     LSTD_ERR_READING_FILE   112
-#define LEV_LSTD_ERR_READING_FILE   ERR
-#define TXT_LSTD_ERR_READING_FILE   "error reading file %s" 
+#define     LSTD_ERR_READING_FILE         112
+#define LEV_LSTD_ERR_READING_FILE         ERR
+#define TXT_LSTD_ERR_READING_FILE         "error reading file %s" 
 
-#define     LSTD_ERR_BAD_FD         113
-#define LEV_LSTD_ERR_BAD_FD         ERR
-#define TXT_LSTD_ERR_BAD_FD         "function %s() called with bad file descriptor"
+#define     LSTD_ERR_BAD_FD               113
+#define LEV_LSTD_ERR_BAD_FD               ERR
+#define TXT_LSTD_ERR_BAD_FD               "function %s() called with bad file descriptor"
 
-#define     LSTD_OPEN_DIR_FAILED    114
-#define LEV_LSTD_OPEN_DIR_FAILED    ERR
-#define TXT_LSTD_OPEN_DIR_FAILED    "cannot open directory %s"
+#define     LSTD_OPEN_DIR_FAILED          114
+#define LEV_LSTD_OPEN_DIR_FAILED          ERR
+#define TXT_LSTD_OPEN_DIR_FAILED          "cannot open directory %s"
 
-#define     LSTD_FORK_PROCESS    115
-#define LEV_LSTD_FORK_PROCESS    INF
-#define TXT_LSTD_FORK_PROCESS    "forking process"
+#define     LSTD_FORK_PROCESS             115
+#define LEV_LSTD_FORK_PROCESS             INF
+#define TXT_LSTD_FORK_PROCESS             "forking process"
 
-#define     LSTD_FORK_PARENT    116
-#define LEV_LSTD_FORK_PARENT    INF
-#define TXT_LSTD_FORK_PARENT    "parent process"
+#define     LSTD_FORK_PARENT              116
+#define LEV_LSTD_FORK_PARENT              INF
+#define TXT_LSTD_FORK_PARENT              "parent process"
 
-#define     LSTD_FORK_CHILD    117
-#define LEV_LSTD_FORK_CHILD    INF
-#define TXT_LSTD_FORK_CHILD    "child process"
+#define     LSTD_FORK_CHILD               117
+#define LEV_LSTD_FORK_CHILD               INF
+#define TXT_LSTD_FORK_CHILD               "child process"
 
-#define     LSTD_FORK_FAILED    118
-#define LEV_LSTD_FORK_FAILED    CRI
-#define TXT_LSTD_FORK_FAILED    "fork failed"
+#define     LSTD_FORK_FAILED              118
+#define LEV_LSTD_FORK_FAILED              CRI
+#define TXT_LSTD_FORK_FAILED              "fork failed"
 
-#define     LSTD_MAKE_DIR_FAILED    119
-#define LEV_LSTD_MAKE_DIR_FAILED    ERR
-#define TXT_LSTD_MAKE_DIR_FAILED    "make direrctory %s failed"
+#define     LSTD_MAKE_DIR_FAILED          119
+#define LEV_LSTD_MAKE_DIR_FAILED          ERR
+#define TXT_LSTD_MAKE_DIR_FAILED          "make direrctory %s failed"
 
-#define     LSTD_MEM_ALLOC_ERROR    120
-#define LEV_LSTD_MEM_ALLOC_ERROR    CRI
-#define TXT_LSTD_MEM_ALLOC_ERROR    "can not allocate memory"
+#define     LSTD_MEM_ALLOC_ERROR          120
+#define LEV_LSTD_MEM_ALLOC_ERROR          CRI
+#define TXT_LSTD_MEM_ALLOC_ERROR          "can not allocate memory"
 
-#define     LSTD_FREE_NULL_ERROR    121
-#define LEV_LSTD_FREE_NULL_ERROR    ERR
-#define TXT_LSTD_FREE_NULL_ERROR    "free call on NULL pointer"
+#define     LSTD_FREE_NULL_ERROR          121
+#define LEV_LSTD_FREE_NULL_ERROR          ERR
+#define TXT_LSTD_FREE_NULL_ERROR          "free call on NULL pointer"
 
-#define     LSTD_GETHOST_BY_NAME_ERROR  122
-#define LEV_LSTD_GETHOST_BY_NAME_ERROR  ERR
-#define TXT_LSTD_GETHOST_BY_NAME_ERROR  "Cannot determinate IP Adress (gethostbyname) %s"
+#define     LSTD_GETHOST_BY_NAME_ERROR    122
+#define LEV_LSTD_GETHOST_BY_NAME_ERROR    ERR
+#define TXT_LSTD_GETHOST_BY_NAME_ERROR    "Cannot determinate IP Adress (gethostbyname) %s"
 
-#define     LSTD_GETENV_ERROR    123
-#define LEV_LSTD_GETENV_ERROR    ERR
-#define TXT_LSTD_GETENV_ERROR    "Cannot get environment variable %s"
+#define     LSTD_GETENV_ERROR             123
+#define LEV_LSTD_GETENV_ERROR             ERR
+#define TXT_LSTD_GETENV_ERROR             "Cannot get environment variable %s"
 
-#define     LSTD_GETENV_WARNING   124
-#define LEV_LSTD_GETENV_WARNING   WAR
-#define TXT_LSTD_GETENV_WARNING   "Cannot get environment variable %s"
+#define     LSTD_GETENV_WARNING           124
+#define LEV_LSTD_GETENV_WARNING           WAR
+#define TXT_LSTD_GETENV_WARNING           "Cannot get environment variable %s"
 
-#define     LSTD_UNKNOWN_CMDLN_ATTR   125
-#define LEV_LSTD_UNKNOWN_CMDLN_ATTR   WAR
-#define TXT_LSTD_UNKNOWN_CMDLN_ATTR   "command line attribute %s not found"
+#define     LSTD_UNKNOWN_CMDLN_ATTR       125
+#define LEV_LSTD_UNKNOWN_CMDLN_ATTR       WAR
+#define TXT_LSTD_UNKNOWN_CMDLN_ATTR       "command line attribute %s not found"
 
-#define     LSTD_UNKNOWN_INI_ATTR   126
-#define LEV_LSTD_UNKNOWN_INI_ATTR   WAR
-#define TXT_LSTD_UNKNOWN_INI_ATTR   "ini file attribute %s not found"
+#define     LSTD_UNKNOWN_INI_ATTR         126
+#define LEV_LSTD_UNKNOWN_INI_ATTR         WAR
+#define TXT_LSTD_UNKNOWN_INI_ATTR         "ini file attribute %s not found"
 
 #define     LSTD_UNKNOWN_CMDLN_ATTR_ERR   127
 #define LEV_LSTD_UNKNOWN_CMDLN_ATTR_ERR   WAR
 #define TXT_LSTD_UNKNOWN_CMDLN_ATTR_ERR   "command line attribute %s not found"
 
-#define     LSTD_UNKNOWN_INI_ATTR_ERR   128
-#define LEV_LSTD_UNKNOWN_INI_ATTR_ERR   ERR
-#define TXT_LSTD_UNKNOWN_INI_ATTR_ERR   "ini file attribute %s not found"
+#define     LSTD_UNKNOWN_INI_ATTR_ERR     128
+#define LEV_LSTD_UNKNOWN_INI_ATTR_ERR     ERR
+#define TXT_LSTD_UNKNOWN_INI_ATTR_ERR     "ini file attribute %s not found"
 
-#define     LSTD_FILE_COPIED   129
-#define LEV_LSTD_FILE_COPIED   LOG
-#define TXT_LSTD_FILE_COPIED   "file %s copied to %s" 
+#define     LSTD_FILE_COPIED              129
+#define LEV_LSTD_FILE_COPIED              LOG
+#define TXT_LSTD_FILE_COPIED              "file %s copied to %s" 
 
-#define     LSTD_OPEN_PIPE_FAILED   130
-#define LEV_LSTD_OPEN_PIPE_FAILED   ERR
-#define TXT_LSTD_OPEN_PIPE_FAILED   "failed to open pipe"
+#define     LSTD_OPEN_PIPE_FAILED         130
+#define LEV_LSTD_OPEN_PIPE_FAILED         ERR
+#define TXT_LSTD_OPEN_PIPE_FAILED         "failed to open pipe"
 
-#define     LSTD_CHILD_ENDED_OK     131
-#define LEV_LSTD_CHILD_ENDED_OK   LOG
-#define TXT_LSTD_CHILD_ENDED_OK   "child %s ended ok"
+#define     LSTD_CHILD_ENDED_OK           131
+#define LEV_LSTD_CHILD_ENDED_OK           LOG
+#define TXT_LSTD_CHILD_ENDED_OK           "child %s ended ok"
 
-#define     LSTD_CHILD_ENDED_ERR   132
-#define LEV_LSTD_CHILD_ENDED_ERR   ERR
-#define TXT_LSTD_CHILD_ENDED_ERR   "child %s ended with error (rc=%d)"
+#define     LSTD_CHILD_ENDED_ERR          132
+#define LEV_LSTD_CHILD_ENDED_ERR          ERR
+#define TXT_LSTD_CHILD_ENDED_ERR          "child %s ended with error (rc=%d)"
 
-#define     LSTD_INI_SYNTAX_ERROR    200
-#define LEV_LSTD_INI_SYNTAX_ERROR    ERR
-#define TXT_LSTD_INI_SYNTAX_ERROR    "ini file syntax error near %s" 
+#define     LSTD_ERR_WRITING_FILE         132
+#define LEV_LSTD_ERR_WRITING_FILE         ERR
+#define TXT_LSTD_ERR_WRITING_FILE         "error writing file %s" 
+
+#define     LSTD_INI_SYNTAX_ERROR         200
+#define LEV_LSTD_INI_SYNTAX_ERROR         ERR
+#define TXT_LSTD_INI_SYNTAX_ERROR         "ini file syntax error near %s" 
 
 #define     LSTD_INI_OPEN_TAG_ERROR  201
 #define LEV_LSTD_INI_OPEN_TAG_ERROR  ERR
