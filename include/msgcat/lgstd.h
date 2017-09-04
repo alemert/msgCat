7d919fe210e8720efd729d46873d97f03f4f9601
Ales Mertuek <ales.mertuek@deutsche-boerse.com>
2017-09-04 13:11:50 +0000

    0.14.01 new messages added
    git-svn-id: http://syspver1/svn/develop/modules/msgCat@2830 72f11a4d-5a25-4e8a-b8c8-85b0af744ad3

--- include/msgcat/lgstd.h
+++ include/msgcat/lgstd.h
@@ -143,6 +143,10 @@
 #define LEV_LSTD_MAKE_DIR_OK          LOG
 #define TXT_LSTD_MAKE_DIR_OK          "make direrctory %s failed"
 
+#define     LSTD_FILE_COPY_ERR              134
+#define LEV_LSTD_FILE_COPY_ERR              ERR
+#define TXT_LSTD_FILE_COPY_ERR              "failed to copy file %s to %s" 
+
 #define     LSTD_INI_SYNTAX_ERROR         200
 #define LEV_LSTD_INI_SYNTAX_ERROR         ERR
 #define TXT_LSTD_INI_SYNTAX_ERROR         "ini file syntax error near %s" 
